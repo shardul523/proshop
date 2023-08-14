10c8ef9edb368fab775608a1ce9d34301aba95c3
Shardul Sisodiya <98705625+shardul523@users.noreply.github.com>
2023-08-14 23:35:22 +0530

    Initial commit for backend

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import products from './data/products.js'
+
+const app = express()
+const PORT = 5000
+
+app.get('/', (req, res) => {
+    res.send('API is running...')
+})
+
+app.get('/api/products', (req, res) => {
+    res.json(products)
+})
+
+app.get('/api/product/:productId', (req, res) => {
+    const product = products.find(product => product._id === req.params.productId)
+    res.json(product)
+})
+
+app.listen(PORT, () => console.log('Server running on port ' + PORT))
