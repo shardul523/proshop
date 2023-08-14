e449d1366d488f3073cf6b5b7ccc78afbb2aabc1
Shardul Sisodiya <98705625+shardul523@users.noreply.github.com>
2023-08-14 23:08:50 +0530

    Completed styling Product Details Page

---
+++ frontend/src/components/ProductDetails.jsx
@@ -0,0 +1,55 @@
+import {
+  SimpleGrid,
+  Image,
+  Heading,
+  Box,
+  Divider,
+  Text,
+  Flex,
+  Card,
+  CardBody,
+  Button,
+} from "@chakra-ui/react";
+import products from "../products";
+import Rating from "./Rating";
+
+export default function ProductDetails({ productId }) {
+  const product = products.find((product) => product._id === productId);
+  const inStock = product.countInStock > 0;
+
+  return (
+    <SimpleGrid columns={[1, null, 3]} spacing={10}>
+      <Image rounded={10} src={product.image} />
+      <Flex flexDir={"column"} gap={5}>
+        <Heading as={"h3"} size={"lg"}>
+          {product.name}
+        </Heading>
+        <Divider />
+        <Rating numReviews={product.numReviews} rating={product.rating} />
+        <Divider />
+        <Text>{product.description}</Text>
+      </Flex>
+      <Card maxW={"container.sm"}>
+        <CardBody
+          display={"flex"}
+          flexDir={"column"}
+          justifyContent={"space-around"}
+        >
+          <Flex justify={"space-between"}>
+            <Text>Price:</Text>
+            <Text>${product.price}</Text>
+          </Flex>
+          <Divider />
+          <Flex justify={"space-between"}>
+            <Text>Status:</Text>
+            <Text>{inStock ? "In Stock" : "Out of Stock!"}</Text>
+          </Flex>
+          <Divider />
+          <Button colorScheme="teal" isDisabled={!inStock}>
+            Add to Cart
+          </Button>
+        </CardBody>
+      </Card>
+    </SimpleGrid>
+  );
+}
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,7 +1,14 @@
 import { useParams } from "react-router-dom";
+import { Button, Flex } from "@chakra-ui/react";
+import ProductDetails from "../components/ProductDetails";
 
 export default function Product() {
   const { productId } = useParams();
 
-  return <div>Product {productId}</div>;
+  return (
+    <Flex flexDir={"column"} gap={10} p={10}>
+      <Button maxW={"fit-content"}>Go Back</Button>
+      <ProductDetails productId={productId} />
+    </Flex>
+  );
 }
