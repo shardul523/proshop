1cc2f6ee9af8ba1f9aa8193534f53a1a019f1302
Shardul Sisodiya <98705625+shardul523@users.noreply.github.com>
2024-03-26 22:37:58 +0530

    Removed and Refactored the code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import { Outlet } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
-import Footer from "./components/common/Footer";
-import Header from "./components/common/Header";
+import Footer from "./components/ui/Footer";
+import Header from "./components/ui/Header";
 
 function App() {
   return (
-    <div className=" grid grid-rows-[auto_1fr_auto] min-h-screen">
+    <div className=" grid grid-rows-[auto_1fr_auto] min-h-screen w-full">
       <Header />
-      <main className="my-5 overflow-auto px-10 mx-auto max-w-screen-xl w-screen">
+      <main className="my-5 overflow-auto mx-auto max-w-screen-xl">
         <Outlet />
       </main>
       <Footer />
--- frontend/src/components/ui/Header.jsx
+++ frontend/src/components/ui/Header.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import { FaShoppingCart } from "react-icons/fa";
 import { FaUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 import Hamburger from "./Hamburger";
 import Navbar from "./Navbar";
 import Navlink from "./Navlink";
-import logo from "../../assets/logo.png";
-import { Link } from "react-router-dom";
-import Badge from "./Badge";
-import { useAllItemsQty, useProfile } from "../../hooks";
+import Badge from "../common/Badge";
 import UserNav from "../user/UserNav";
+
+import { useAllItemsQty, useProfile } from "../../hooks";
+import logo from "../../assets/logo.png";
 
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
@@ -19,7 +21,7 @@ function Header() {
 
   return (
     <header className="bg-primary-600 text-secondary-200">
-      <div className="sm:flex justify-between lg:mx-auto max-w-screen-xl px-4 py-3 space-y-7 sm:space-y-0">
+      <div className="sm:flex justify-between lg:mx-auto py-3 space-y-7 sm:space-y-0 px-10 max-w-screen-xl">
         <div className="flex justify-between items-center">
           <Link to={"/"} className="text-xl flex items-center">
             <img src={logo} alt="brand" /> <span>ProShop</span>
@@ -27,7 +29,7 @@ function Header() {
           <Hamburger isOpen={isOpen} toggleOpen={toggleOpen} />
         </div>
         <Navbar isOpen={isOpen}>
-          <ul className="flex flex-col sm:flex-row items-start sm:items-center sm:gap-8 space-y-5 sm:space-y-0 ml-3">
+          <ul className="flex flex-col sm:flex-row items-start sm:items-center sm:gap-5 space-y-5 sm:space-y-0 ml-3">
             <li className="relative">
               <Navlink to={"/cart"}>
                 <FaShoppingCart /> Cart
