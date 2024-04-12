b154769028b5ce1c5c64a0c15f4612705505be28
Shardul Sisodiya <98705625+shardul523@users.noreply.github.com>
2024-04-12 20:26:37 +0530

    Added Shadcn for styling

--- frontend/src/components/user/UserNav.jsx
+++ frontend/src/components/user/UserNav.jsx
@@ -3,11 +3,19 @@ import { FaUser } from "react-icons/fa";
 import { toast } from "react-hot-toast";
 import { useDispatch } from "react-redux";
 import { useQueryClient } from "@tanstack/react-query";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { IoMdArrowDropdown } from "react-icons/io";
 
 import Nav from "../common/Nav";
 
 import { unauthenticate } from "./authSlice";
 import { logout } from "../../services/authApi";
+import Button from "../common/Button";
 
 function UserNav({ name }) {
   const dispatch = useDispatch();
@@ -38,24 +46,41 @@ function UserNav({ name }) {
   };
 
   return (
-    <Nav
-      navTitle={
-        <>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className="text-lg hover:bg-primary-700 py-1 px-2 rounded-lg flex items-center gap-1 z-50">
           <FaUser />
-          {name}
-        </>
-      }
-    >
-      <Link to={"/profile"} className="dropdown-link">
-        Profile
-      </Link>
-      <button
-        onClick={onLogoutHandler}
-        className="dropdown-link w-full text-left"
-      >
-        Logout
-      </button>
-    </Nav>
+          <span>{name}</span>
+          <IoMdArrowDropdown />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-40">
+        <DropdownMenuItem>
+          <Link to={"/profile"}>Profile</Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <button onClick={onLogoutHandler}>Logout</button>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+    // <Nav
+    //   navTitle={
+    //     <>
+    //       <FaUser />
+    //       {name}
+    //     </>
+    //   }
+    // >
+    //   <Link to={"/profile"} className="dropdown-link">
+    //     Profile
+    //   </Link>
+    //   <button
+    //     onClick={onLogoutHandler}
+    //     className="dropdown-link w-full text-left"
+    //   >
+    //     Logout
+    //   </button>
+    // </Nav>
   );
 }
 export default UserNav;
